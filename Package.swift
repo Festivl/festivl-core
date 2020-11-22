89275fc1408595ff1a4bef5b1f3dc96d7266bd68
Woody <woodymelling@gmail.com>
2020-11-21 20:53:43 -0700

    Change name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "festivl-core",
+    name: "FestivlCore",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "festivl-core",
-            targets: ["festivl-core"]),
+            name: "FestivlCore",
+            targets: ["FestivlCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "festivl-core",
+            name: "FestivlCore",
             dependencies: []),
         .testTarget(
-            name: "festivl-coreTests",
-            dependencies: ["festivl-coreTests"]),
+            name: "FestivlCoreTests",
+            dependencies: ["FestivlCore"]),
     ]
 )
