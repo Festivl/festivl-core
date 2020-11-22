ab86c17833a5cd02c6952a34979e2d26033304d1
Woody <woodymelling@gmail.com>
2020-11-21 21:02:06 -0700

    Name change again

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "FestivlCore",
+    name: "festivl-core",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
