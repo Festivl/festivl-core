19e5b4d9bc14bbe5247032efc9ac2771b8abe79e
Woody <woodymelling@gmail.com>
2020-12-31 22:52:47 -0700

    Remove slug from festival

--- Sources/FestivlCore/ViewModels/Festival/Festival.swift
+++ Sources/FestivlCore/ViewModels/Festival/Festival.swift
@@ -10,13 +10,11 @@ import Foundation
 public struct Festival: Codable {
     public let id: UUID?
     public let name: String
-    public let urlName: String
     public let iterations: [FestivalIteration]
 
     public init(id: UUID?, name: String, urlName: String, iterations: [FestivalIteration]) {
         self.id = id
         self.name = name
-        self.urlName = urlName
         self.iterations = iterations
     }
 }
