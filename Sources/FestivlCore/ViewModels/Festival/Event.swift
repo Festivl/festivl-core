f95e730e8dcd1a8adec4540e983501b130686e4d
Woody <woodymelling@gmail.com>
2021-01-24 15:20:49 -0700

    Event model updates

--- Sources/FestivlCore/ViewModels/Festival/Event.swift
+++ Sources/FestivlCore/ViewModels/Festival/Event.swift
@@ -13,15 +13,19 @@ public struct Event: Codable {
     public let name: String
     public let startDate: Date
     public let endDate: Date
+    public let dayStartsAtNoon: Bool
+    public let imageURL: String?
     public let stages: [Stage]
 
-    public init(id: UUID?, festivalID: UUID, name: String, startDate: Date, endDate: Date, stages: [Stage]) {
+    public init(id: UUID?, festivalID: UUID, name: String, startDate: Date, endDate: Date, imageURL: String?, dayStartsAtNoon: Bool, stages: [Stage]) {
         self.id = id
         self.festivalID = festivalID
         self.name = name
         self.startDate = startDate
         self.endDate = endDate
         self.stages = stages
+        self.dayStartsAtNoon = dayStartsAtNoon
+        self.imageURL = imageURL
     }
 }
 
